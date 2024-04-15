b43e5ceccda3dc1d892803b87f50a2a6e15f05e3
Qurat-ul-ain <qurat1356@gmail.com>
2024-04-15 17:57:17 +0500

    Second initial for Components and Assets

--- Landing Page/src/App.jsx
+++ Landing Page/src/App.jsx
@@ -1,5 +1,13 @@
+import { useState } from "react"
 
 const App = () => {
+  let HeroData = [
+    {text1:"Be the", text2:"best version of yourself"},
+    {text1:"Get the", text2:"aspiring attitude"},
+    {text1:"Follow", text2:"your passion"}
+  ]
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
   return (
     <div></div>
   )
