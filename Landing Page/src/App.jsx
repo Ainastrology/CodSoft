5d514151d2adcd883df9d42a6f8a33e0e3a57e33
Qurat-ul-ain <qurat1356@gmail.com>
2024-04-17 19:51:18 +0500

    Landing Page Final Commit
    This include the navbar section, the background section using React.

--- Landing Page/src/App.jsx
+++ Landing Page/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from "react"
+import Background from "./Components/Background/Background";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import { useEffect } from "react";
 
 const App = () => {
   let HeroData = [
@@ -6,10 +10,27 @@ const App = () => {
     {text1:"Get the", text2:"aspiring attitude"},
     {text1:"Follow", text2:"your passion"}
   ]
-  const [heroCount, setHeroCount] = useState(2);
+  const [heroCount, setHeroCount] = useState(0);
   const [playStatus, setPlayStatus] = useState(false);
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setHeroCount((count)=>{return count===2?0:count+1})
+    }, 3000)
+  },[])
+
   return (
-    <div></div> 
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/> 
+      <Navbar/>
+      <Hero
+      setPlayStatus={setPlayStatus}
+      HeroData={HeroData[heroCount]}
+      heroCount={heroCount}
+      setHeroCount={setHeroCount}
+      playStatus={playStatus}
+      />
+    </div> 
   )
 }
 
