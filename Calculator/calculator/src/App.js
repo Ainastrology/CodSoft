c1b9a6cc17e243c8378d358ed4383ed49bb38cd4
Qurat-ul-ain <qurat1356@gmail.com>
2024-04-20 23:23:54 +0500

    Calculator Initial code

---
+++ Calculator/calculator/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+function App() {
+    const [result, setResult] = useState("Hello")
+  return (
+    <div>
+        <div></div>
+    </div>
+  )
+}
+
+export default App
